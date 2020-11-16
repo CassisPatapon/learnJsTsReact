8a28e5b52da44ed588a708805a3cfc10a62d00c8
Sandra Monnier <70577952+SandraMonnier@users.noreply.github.com>
2020-11-16 14:33:37 +0100

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import * as React from "react";
 import { render } from "react-dom";
 
 import App from "./App";
+import LikeButton from "./LikeButton";
 
 const rootElement = document.getElementById("root");
 render(<App />, rootElement);
+
+const likeButtonContainer = document.getElementById("like_button_container");
+render(<LikeButton />, likeButtonContainer);
